3c062808f3db998b4b0194564f623ee00abe00b3
JordanRobo <hello.world@jordanrobo.xyz>
2025-07-25 13:15:45 +1000

    fix: updated config file for production deployment

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,9 +2,10 @@ import * as path from "node:path";
 import { defineConfig } from "rspress/config";
 
 export default defineConfig({
+	base: '/datalayer-docs/',
 	root: path.join(__dirname, "docs"),
 	globalStyles: path.join(__dirname, "styles/index.css"),
-	title: "Adobe Experience Platform",
+	title: "Martech Docs",
 	icon: "https://fav.farm/%E2%98%95",
 	logo: {
 		light: "/logo-dark.png",
